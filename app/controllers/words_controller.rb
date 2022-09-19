b1b631dfdbbe1b462ba71c3c63bea13af95e1ba9
RobynSpaulding <robynm27@gmail.com>
2022-09-18 20:35:37 -0700

    encrypts api key

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -4,7 +4,7 @@ class WordsController < ApplicationController
   def index
     response = Unirest.get("https://wordsapiv1.p.mashape.com/words/happy",
     headers:{
-      "X-Mashape-Key" => "<d2549d880amsh1c69053bac09586p147fd6jsn2e9ade2c341f>",
+      "X-Mashape-Key" => "<#{Rails.application.credentials.words_api_key}>",
       "Accept" => "application/json"})
     # render json: response.parse(:json)
   end
